2d0abd8fa9378ac7d7a7b317e2c13888dea9b1e0
eduardobaia <eduardo_baia@msn.com>
2021-07-03 17:07:09 +0100

    to do app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { AddNewTaskPage } from './../add-new-task/add-new-task.page';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,51 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  todoList = [
+    {
+    itemName: 'Coding',
+    itemDueDate: '10-10-21',
+    itemPriority: 'High',
+    itemCategory: 'work'
+  },
+  {
+    itemName: 'Design',
+    itemDueDate: '01-10-21',
+    itemPriority: 'High',
+    itemCategory: 'work'
+  },
+  {
+    itemName: 'Coding',
+    itemDueDate: '02-10-21',
+    itemPriority: 'Middle',
+    itemCategory: 'personal'
+  },
+  {
+    itemName: 'Workout',
+    itemDueDate: '07-10-21',
+    itemPriority: 'Low',
+    itemCategory: 'personal'
+  }]
+
+  today: number = Date.now();
+  constructor(private modalCtrl: ModalController) { }
+
+  async addTask() {
+    const modal = await this.modalCtrl.create({
+      component: AddNewTaskPage
+    });
+
+
+    modal.onDidDismiss().then(newTaskObjt => {
+      console.log(newTaskObjt);
+      this.todoList.push(newTaskObjt.data);
+    })
+
+    return await modal.present();
+  }
+
+  delete(index){
+    this.todoList.splice(index, 1)
+  }
 
 }
